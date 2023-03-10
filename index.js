c3cc563e4a5b1d8ce263cb147ed4767017986cd7
Stephen Jayakar <aj12ay@gmail.com>
2023-03-09 19:10:26 -0800

    honestly not bad. moves down

--- index.js
+++ index.js
@@ -1,44 +1,49 @@
-const canvas = document.getElementById('myCanvas');
-const ctx = canvas.getContext('2d');
+const canvas = document.getElementById("myCanvas");
+const ctx = canvas.getContext("2d");
 const canvasLeft = canvas.offsetLeft + canvas.clientLeft,
-      canvasTop = canvas.offsetTop + canvas.clientTop;
+  canvasTop = canvas.offsetTop + canvas.clientTop;
 /* if (canvas.getContext) {
-* 
-* } */
+ *
+ * } */
+
+const Y_VELOCITY = 1;
 
 const RECT_WIDTH = 75;
 const RECT_HEIGHT = 75;
 function drawRect(x, y) {
-    ctx.fillRect(x, y, RECT_WIDTH, RECT_HEIGHT);
+  ctx.fillRect(x, y, RECT_WIDTH, RECT_HEIGHT);
+}
+function clearRect(x, y) {
+  ctx.clearRect(x, y, RECT_WIDTH, RECT_HEIGHT);
 }
 
-canvas.addEventListener('click', function(event) {
-  const x = event.pageX - canvasLeft,
-        y = event.pageY - canvasTop;
+canvas.addEventListener(
+  "click",
+  (event) => {
+    const x = event.pageX - canvasLeft,
+      y = event.pageY - canvasTop;
 
-  console.log(x, y);
-    if (x >= rect.x &&
-        x < rect.x + RECT_WIDTH &&
-        y >= rect.y &&
-        y < rect.y + RECT_HEIGHT) {
-        console.log('hit!')
+    console.log(x, y);
+    if (
+      x >= rect.x &&
+      x < rect.x + RECT_WIDTH &&
+      y >= rect.y &&
+      y < rect.y + RECT_HEIGHT
+    ) {
+      // hit!
+      setInterval(() => {
+        clearRect(rect.x, rect.y);
+        rect.y += Y_VELOCITY;
+        drawRect(rect.x, rect.y);
+      }, 1000 / 120);
     }
 
-    /* /* var x = event.pageX - elemLeft,
-*  y = event.pageY - elemTop; */
-
- // Collision detection between clicked offset and element.
-    /* elements.forEach(function(element) {
-   *     if (y > element.top && y < element.top + element.height 
-   *         && x > element.left && x < element.left + element.width) {
-   *         alert('clicked an element');
-   *     } */
-// });
-
-}, false);
+  },
+  false
+);
 
 let rect = {
   x: 25,
   y: 25,
-}
-drawRect(25, 25)
+};
+drawRect(25, 25);
