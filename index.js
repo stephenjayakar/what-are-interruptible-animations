2b3d83ef1652f0451229591c8ffcaeacf58bc1c1
Stephen Jayakar <aj12ay@gmail.com>
2023-03-09 17:37:34 -0800

    box is clickable

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const canvas = document.getElementById('myCanvas');
 const ctx = canvas.getContext('2d');
+const canvasLeft = canvas.offsetLeft + canvas.clientLeft,
+      canvasTop = canvas.offsetTop + canvas.clientTop;
 /* if (canvas.getContext) {
 * 
 * } */
@@ -7,23 +9,36 @@ const ctx = canvas.getContext('2d');
 const RECT_WIDTH = 75;
 const RECT_HEIGHT = 75;
 function drawRect(x, y) {
-    ctx.fillRect(x, y, x + RECT_WIDTH, y + RECT_HEIGHT);
+    ctx.fillRect(x, y, RECT_WIDTH, RECT_HEIGHT);
 }
 
 canvas.addEventListener('click', function(event) {
-    console.log(event);
-    /* var x = event.pageX - elemLeft,
-*     y = event.pageY - elemTop;
+  const x = event.pageX - canvasLeft,
+        y = event.pageY - canvasTop;
 
-* // Collision detection between clicked offset and element.
-* elements.forEach(function(element) {
-*     if (y > element.top && y < element.top + element.height 
-*         && x > element.left && x < element.left + element.width) {
-*         alert('clicked an element');
-*     }
-* }); */
+  console.log(x, y);
+    if (x >= rect.x &&
+        x < rect.x + RECT_WIDTH &&
+        y >= rect.y &&
+        y < rect.y + RECT_HEIGHT) {
+        console.log('hit!')
+    }
+
+    /* /* var x = event.pageX - elemLeft,
+*  y = event.pageY - elemTop; */
+
+ // Collision detection between clicked offset and element.
+    /* elements.forEach(function(element) {
+   *     if (y > element.top && y < element.top + element.height 
+   *         && x > element.left && x < element.left + element.width) {
+   *         alert('clicked an element');
+   *     } */
+// });
 
 }, false);
 
-
+let rect = {
+  x: 25,
+  y: 25,
+}
 drawRect(25, 25)
