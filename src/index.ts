a1f394e708429a43c10ac56f635a8a21f94fb12e
Stephen Jayakar <aj12ay@gmail.com>
2023-03-12 23:04:54 -0700

    add a simple gravity acceleration curve

--- src/index.ts
+++ src/index.ts
@@ -5,16 +5,27 @@ import {
   camera,
 } from './setup';
 
+const acceleration = 9.81 / 100; // 9.81 m/s
+let velocity = 0;
+
 mesh.position.y = 100;
 function render() {
   requestAnimationFrame(render);
 
-  mesh.position.x += 0.1;
-  if (mesh.position.x > 5) {
-    mesh.position.x = -5;
+  velocity += acceleration
+
+  mesh.position.y -= velocity;
+
+  if (mesh.position.y < -100) {
+    resetScene()
   }
 
   renderer.render(scene, camera);
+}
+
+function resetScene() {
+  mesh.position.y = 100;
+  velocity = 0;
 }
 
 render();
