ebe4c7dbb84b4fce7b7188aa99e9b3cf0efcfb73
Stephen Jayakar <aj12ay@gmail.com>
2023-03-17 12:44:25 -0700

    ugh it is flying off

--- src/rubber-banding.ts
+++ src/rubber-banding.ts
@@ -3,13 +3,13 @@ import { Mesh } from "three";
 enum ANIM_STATE {
   FALLING,
   BANDING,
-};
+}
 
 // Consts
 const floorY = -100;
-const mass = 10;
+const mass = 100;
 const k = 10;
-const damping = 0.5;
+const damping = 0.9;
 // const ceilY = 200;
 // const EPS = 0.0000001;
 
@@ -28,18 +28,25 @@ class RubberBandingAnimation {
     this.velocity += this.acceleration * timeDelta;
     mesh.position.y += this.velocity * timeDelta;
     if (this.anim_state == ANIM_STATE.FALLING) {
-        // Check if the square has hit the floor and apply rubber banding
-        if (mesh.position.y <= floorY) {
-          this.anim_state = ANIM_STATE.BANDING;
-        }
+      // Check if the square has hit the floor and apply rubber banding
+      if (mesh.position.y <= floorY) {
+        this.anim_state = ANIM_STATE.BANDING;
+      }
     } else {
+      /// f = m * a
+      const currentF = mass * this.acceleration;
       const x = floorY - mesh.position.y;
-      let F = (k * x);
+      let springF = k * x;
       // TODO: how to do damping properly?
-      F -= F * damping;
-      const a = (F / mass);
+      // springF -= springF * damping;
+      const F = (currentF + springF);
+      const a = F / mass;
       this.acceleration = a;
-        console.log(a);
+      console.log({
+        a,
+        currentF,
+        springF,
+      });
     }
   };
 }
