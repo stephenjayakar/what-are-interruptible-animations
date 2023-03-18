4b263773df8fd81501fa7974aa050b0f4cbdb1f7
Stephen Jayakar <aj12ay@gmail.com>
2023-03-17 21:54:03 -0500

    Squashed commit of the following:
    commit 565934756caf6f7ce4fd960706e978f971dd684e
    Author: Stephen Jayakar <aj12ay@gmail.com>
    Date:   Fri Mar 17 21:52:58 2023 -0500
    
        finally got a working rubber banding model
    
    commit ebe4c7dbb84b4fce7b7188aa99e9b3cf0efcfb73
    Author: Stephen Jayakar <aj12ay@gmail.com>
    Date:   Fri Mar 17 12:44:25 2023 -0700
    
        ugh it is flying off
    
    commit f9b686884c7922d71d52ea959fa54b6f464c6fe9
    Author: Stephen Jayakar <aj12ay@gmail.com>
    Date:   Fri Mar 17 12:28:24 2023 -0700
    
        can't figure out damping. encoded hooke's law though

--- src/rubber-banding.ts
+++ src/rubber-banding.ts
@@ -1,30 +1,49 @@
 import { Mesh } from "three";
 
+enum ANIM_STATE {
+  FALLING,
+  BANDING,
+}
+
 // Consts
 const floorY = -100;
-const rubberBandFactor = 0.2;
+const mass = 1.5;
+const k = 8;
+// Critical damping
+const c = 2 * Math.sqrt(mass * k);
+
 // const ceilY = 200;
 // const EPS = 0.0000001;
 
 class RubberBandingAnimation {
   acceleration = 0;
   velocity = 0;
+  anim_state = ANIM_STATE.FALLING;
   reset = (mesh: Mesh) => {
     mesh.position.y = 200;
     this.velocity = 0;
     this.acceleration = -9.81 * 100;
+    this.anim_state = ANIM_STATE.FALLING;
   };
 
   render = (mesh: Mesh, timeDelta: number) => {
     this.velocity += this.acceleration * timeDelta;
     mesh.position.y += this.velocity * timeDelta;
-
-    // Check if the square has hit the floor and apply rubber banding
-    if (mesh.position.y <= floorY) {
-      const diffY = mesh.position.y - floorY;
-      this.velocity = -(diffY * rubberBandFactor);
-      // mesh.position.y = floorY + (mesh.position.y - floorY) * rubberBandFactor;
-// this.velocity = -this.velocity * rubberBandFactor;
+    if (this.anim_state == ANIM_STATE.FALLING) {
+      // Check if the square has hit the floor and apply rubber banding
+      if (mesh.position.y <= floorY) {
+        this.anim_state = ANIM_STATE.BANDING;
+        this.acceleration = 0;
+      }
+    } else {
+      /// f = m * a
+      // const currentF = mass * this.acceleration;
+      const x = floorY - mesh.position.y;
+      let springF = k * x;
+      const dampingF = c * this.velocity;
+      const F = springF - dampingF;
+      const a = F / mass;
+      this.acceleration = a;
     }
   };
 }
